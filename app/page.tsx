c665ac304711c7bdc0880c10b3464c9b70ea44ee
taiwo298 <iwayemitaiwo884@gmail.com>
2026-08-27 09:45:39 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@/components/header";
 import { Hero } from "@/components/hero";
+
 import { Features } from "@/components/features";
 import { Security } from "@/components/security";
 import { CTA } from "@/components/cta";
